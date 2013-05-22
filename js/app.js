5e03ae9c998e5740753a8a5969b35b55b407bf51
Gilles Bassière <gilles_bassiere@natural-solutions.eu>
2013-05-22 18:44:37 +0200

    Detect or set global parameters during initialization

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 var capturePhoto = (function(app) {
     "use strict";
 	app = {
-		Collections: {},
 		Models: {},
 		Views: {},
 		// Instances
-		collections: {},
 		views: {},
 		models: {},
 		utils: {},
-		global:{},
+		global: {},
 		init: function() {
             var initalizers = [];
+
+            // Load templates (async)
             initalizers.push(
                 $.ajax({
                     url: 'tpl/templates.html',
@@ -20,13 +20,54 @@ var capturePhoto = (function(app) {
                     $("#templates").append(contents);
                 })
             );
+
+            // Detect current geographic position
+            if (navigator.geolocation) {
+                var dfd = $.Deferred();
+                var callbackSuccess = $.proxy(function(position) {
+                    this.global.currentPosition = {
+                        lat: position.coords.latitude,
+                        lon: position.coords.longitude
+                    };
+                    this.dfd.resolve();
+                }, {dfd: dfd, global: app.global});
+                var callbackError = $.proxy(function(position) {
+                    // Don't break initialization if geolocation fail, just rely on default values
+                    this.dfd.resolve();
+                }, {dfd: dfd});
+                navigator.geolocation.getCurrentPosition(
+                    callbackSuccess,
+                    callbackError,
+                    {maximumAge: 200000, enableHighAccuracy: false}
+                );
+            }
+
+            // Detect device width
+            var e = window, a = 'inner';
+            if (!('innerWidth' in window)) {
+                a = 'client';
+                e = document.documentElement || document.body;
+            }
+            app.global.viewportSize = {
+                w: e[a + 'Width'],
+                h: e[a + 'Height']
+            };
+
+            // Start app when everything is ready
             $.when.apply($, initalizers).done(function() {
                 app.router = new app.Router();
                 Backbone.history.start();
             });
         }
 	};
-	
+
+    // General map configuration
+    app.global.mapExtent = {minx:42.3, miny:-5.15, max:51.1, maxy:8.25};
+    app.global.basemapUrl = [
+        'http://a.tile.openstreetmap.fr/osmfr/${z}/${x}/${y}.png',
+        'http://b.tile.openstreetmap.fr/osmfr/${z}/${x}/${y}.png',
+        'http://c.tile.openstreetmap.fr/osmfr/${z}/${x}/${y}.png'
+    ];
 
     if (window.PhoneGap) {
         document.addEventListener("deviceready", app.init, false);
